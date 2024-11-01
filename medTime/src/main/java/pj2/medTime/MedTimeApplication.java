2126a824a30d299604863f28fc898ada276ea225
EzequielSoeiro <ezequielg.soeiro@gmail.com>
2024-10-31 23:19:42 -0300

    commit inicial - criação do projeto maven com springboot

---
+++ medTime/src/main/java/pj2/medTime/MedTimeApplication.java
@@ -0,0 +1,13 @@
+package pj2.medTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedTimeApplication.class, args);
+	}
+
+}
---
+++ medTime/src/test/java/pj2/medTime/MedTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package pj2.medTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

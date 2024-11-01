d8fb7e76d4f002ef6bd92f746623e7dbcfff4f47
EzequielSoeiro <ezequielg.soeiro@gmail.com>
2024-11-01 10:17:10 -0300

    implementei a tela de login
    email padrao: adm@gmail.com
    senha: 123456

--- medTime/src/main/java/pj2/medTime/controller/HomeController.java
+++ medTime/src/main/java/pj2/medTime/controller/HomeController.java
@@ -2,11 +2,9 @@ package pj2.medTime.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
-public class homeController {
+public class HomeController {
     @GetMapping("/home")
     public String home() {
         return "home";

cd19b9a95ef3ccd86af33e2e08fb6ab474fcf680
EzequielSoeiro <ezequielg.soeiro@gmail.com>
2024-11-10 11:26:26 -0300

    nem deus sabe como isso aq ta funcionando
    função editar medicamento

--- medTime/src/main/java/pi2/medTime/controller/MedicamentoController.java
+++ medTime/src/main/java/pi2/medTime/controller/MedicamentoController.java
@@ -1,39 +1,67 @@
-package pi2.medTime.controller;
+    package pi2.medTime.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
-import pi2.medTime.model.Medicamento;
-import pi2.medTime.repository.MedicamentoRepository;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.ModelAttribute;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.servlet.ModelAndView;
+    import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+    import pi2.medTime.model.Medicamento;
+    import pi2.medTime.repository.MedicamentoRepository;
 
-import java.util.ArrayList;
+    import java.util.ArrayList;
+    import java.util.Optional;
 
-@Controller
-public class MedicamentoController {
+    @Controller
+    public class MedicamentoController {
 
-    @Autowired
-    MedicamentoRepository medicamentoRepository;
-    @GetMapping("/home")
-    public ModelAndView home() {
-        ModelAndView mv = new ModelAndView("home");
-        ArrayList<Medicamento> medicamentos = (ArrayList<Medicamento>) medicamentoRepository.findAll();
-        mv.addObject("medicamentos", medicamentos);
-        return mv;
+        @Autowired
+        MedicamentoRepository medicamentoRepository;
+
+
+        @GetMapping("/home")
+        public ModelAndView home() {
+            ModelAndView mv = new ModelAndView("home");
+            ArrayList<Medicamento> medicamentos = (ArrayList<Medicamento>) medicamentoRepository.findAll();
+            mv.addObject("medicamentos", medicamentos);
+            return mv;
+        }
+        @GetMapping("/cadastroDeMedicamento")
+        public String cadastrarMedicamento(){
+            return "cadastroDeMedicamento";
+        }
+
+        @PostMapping("/cadastrarMedicamento")
+        public String  CadastrarMedicamento(Model model, Medicamento medicamento){
+            medicamentoRepository.save(medicamento);
+
+            model.addAttribute("cadastroMedicamentoSucesso", true);
+            return "cadastroDeMedicamento";
+        }
+
+        @GetMapping("/editaMedicamento/{id}")
+        public String getMedicamentoById(@PathVariable Long id, Model model) {
+            Medicamento medicamento = buscarPorId(id);
+            model.addAttribute("medicamento", medicamento);
+            return "editarMedicamento";
+        }
+
+        @PostMapping("/editarMedicamento")
+        public String editarMedicamento(@ModelAttribute("medicamento") Medicamento medicamento) {
+        atualizar(medicamento);
+        return "redirect:/home";
+        }
+
+        public Medicamento buscarPorId(Long id) {
+            return medicamentoRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("Medicamento não encontrado com o ID: " + id));
+        }
+
+        public void atualizar(Medicamento medicamento) {
+            medicamentoRepository.save(medicamento);
+        }
+
     }
-    @GetMapping("/cadastroDeMedicamento")
-    public String cadastrarMedicamento(){
-        return "cadastroDeMedicamento";
-    }
-
-    @PostMapping("/cadastrarMedicamento")
-    public String  CadastrarMedicamento(Model model, Medicamento medicamento){
-        medicamentoRepository.save(medicamento);
-
-        model.addAttribute("cadastroMedicamentoSucesso", true);
-        return "cadastroDeMedicamento";
-    }
-
-}

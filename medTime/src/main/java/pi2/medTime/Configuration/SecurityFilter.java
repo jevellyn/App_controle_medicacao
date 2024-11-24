eb9793ee36b5039ce10c171ac812cfde7d3cb25c
EzequielSoeiro <ezequielg.soeiro@gmail.com>
2024-11-24 11:43:42 -0300

    Tratamento token

--- medTime/src/main/java/pi2/medTime/Configuration/SecurityFilter.java
+++ medTime/src/main/java/pi2/medTime/Configuration/SecurityFilter.java
@@ -27,6 +27,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
+            token = token.trim(); // Remove espaços no início e no final
             var login = tokenService.validateToken(token);
             UserDetails user = usuarioRepository.findByEmail(login);
 
